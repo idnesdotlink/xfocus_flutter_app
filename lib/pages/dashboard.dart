b5227491afb1fcfabd99f85aa5f004b69e0a860b
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2018-08-28 14:23:19 +0700

    various

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -19,7 +19,15 @@ class DashboardPage extends StatefulWidget {
 
 class _DashboardPageState extends State<DashboardPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-
+  Choice _selectedChoice = choices[0];
+  void _select(Choice choice) {
+    debugPrint(choice.toString());
+    setState(
+      () {
+        _selectedChoice = choice;
+      }
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -31,13 +39,28 @@ class _DashboardPageState extends State<DashboardPage> {
               icon: new Icon(Icons.settings),
               onPressed: () => _scaffoldKey.currentState.openDrawer()),
           actions: <Widget>[
-            new IconButton(
-              icon: new Icon(Icons.shopping_cart),
-              tooltip: 'Open shopping cart',
-              onPressed: () {
-                // ...
+            /* new DropdownButton(
+              items: <String>['A'].map((String value) {
+                return new DropdownMenuItem<String>(
+                  value: value,
+                  child: new Text(value)
+                );
+              }).toList(),
+              onChanged: (_) {
+
               },
-            ),
+            ), */
+            PopupMenuButton<Choice>(
+              onSelected: _select,
+              itemBuilder: (BuildContext context) {
+                return choices.skip(2).map((Choice choice) {
+                  return PopupMenuItem<Choice>(
+                    value: choice,
+                    child: Text(choice.title),
+                  );
+                }).toList();
+              }
+            )
           ],
         ),
         drawer: new AppDrawer(),
@@ -45,6 +68,7 @@ class _DashboardPageState extends State<DashboardPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             InfoBox(),
+            Text(_selectedChoice.toString()),
             Expanded(
               child: new CustomScrollView(
                 shrinkWrap: true,
@@ -71,3 +95,17 @@ class _DashboardPageState extends State<DashboardPage> {
         );
   }
 }
+
+class Choice {
+  const Choice({ this.title, this.icon });
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'car', icon: Icons.directions_car),
+  const Choice(title: 'car', icon: Icons.directions_car),
+  const Choice(title: 'car', icon: Icons.directions_car),
+  const Choice(title: 'car', icon: Icons.directions_car),
+  const Choice(title: 'car', icon: Icons.directions_car),
+];
