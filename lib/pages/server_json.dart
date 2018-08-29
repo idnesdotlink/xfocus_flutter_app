af6fc86050278a0a89d28154c75ceec289688b9e
it-22-tip <it.22.tip@gmail.com>
2018-08-30 06:31:41 +0700

    dependencies

--- lib/pages/server_json.dart
+++ lib/pages/server_json.dart
@@ -7,6 +7,36 @@ import 'package:flutter/material.dart';
 // import 'package:archive/archive_io.dart';
 // import 'dart:convert';
 
+Future<String> consolidateHttpClientResponse(HttpClientResponse response) async {
+  final Completer<String> completer = new Completer<String>.sync();
+  final StringBuffer buffer = new StringBuffer();
+
+  response.transform(utf8.decoder).listen((String chunk) {
+    buffer.write(chunk);
+    print(chunk); // all the chunks will get printed
+  }, onDone: () {
+    print('DONE'); // will be printed after last chunk is printed from above
+    print(buffer.toString()); // will not include the last chunk we got above.  callers will be missing some data.
+    completer.complete(buffer.toString()); 
+  }, onError: completer.completeError, cancelOnError: true);
+
+  return completer.future;
+}
+
+Future<String> psg () async {
+final psgbgt = await HttpClient().get('10.0.2.2', 9080, '/dummy')
+     .then((HttpClientRequest request) => request.close())
+     .then((HttpClientResponse response) async {
+       return await consolidateHttpClientResponse(response);
+      //  response.transform(utf8.decoder).listen((contents) {
+      //    // handle data
+      //    debugPrint(contents);
+      //    return contents;
+      //  });
+     });
+  return psgbgt;
+}
+
 Future<Post> fetchPost() async {
   /* final response =
       await http.get('https://jsonplaceholder.typicode.com/posts/1'); */
@@ -18,6 +48,8 @@ Future<Post> fetchPost() async {
   // utf8.encode(response.body);
   // debugPrint(response.bodyBytes);
   debugPrint(response.body);
+
+  // consolidateHttpClientResponse(response);
 
   if (response.statusCode == 200) {
     // If the call to the server was successful, parse the JSON
@@ -54,13 +86,14 @@ class ServerJson extends StatelessWidget {
         title: Text('Fetch Data Example'),
       ),
       body: Center(
-        child: FutureBuilder<Post>(
-          future: fetchPost(),
+        child: FutureBuilder<String>(
+          future: psg(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return Text(snapshot.data.title);
+              debugPrint(snapshot.data);
+              // return Text(snapshot.data.title);
             } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
+              // return Text("${snapshot.error}");
             }
 
             // By default, show a loading spinner
